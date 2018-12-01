25984fc234ac8ea2993876642525917108aed57c
Marcio Satin <marcio.satin@gmail.com>
2018-12-01 18:24:12 +0000

    Validators para refeição

--- src/Cardapium/Controllers/meals-itens.php
+++ src/Cardapium/Controllers/meals-itens.php
@@ -1,39 +1,32 @@
 <?php
 
+use Cardapium\Models\MealsIten;
+use Cardapium\Models\Validators\ValidatorException;
 use Psr\Http\Message\ServerRequestInterface;
 
 $app->get('/meals-itens/{id}/add', function(ServerRequestInterface $request) use($app) {
-            $view = $app->service('view.renderer');
-            $id = (int) $request->getAttribute('id');
-            $repository = $app->service('meal.repository');
-            $repositoryI = $app->service('ingredient.repository');
-            $repositoryS = $app->service('state.repository');
-            $meal = $repository->findOneBy([
-                'id' => $id,
-            ]);
-
-            $types = [
-                ['id' => 1, 'name' => 'Principal'],
-                ['id' => 2, 'name' => 'Acompanhamento'],
-                ['id' => 3, 'name' => 'Salada'],
-                ['id' => 4, 'name' => 'Legume']
-            ];
-
-            $repositoryItem = $app->service('meal-item.repository');
-            $items = $repositoryItem->findByField('meal_id', $meal->id);
-
-            return $view->render('meals-itens/add.html.twig', [
-                        'meal' => $meal,
-                        'ingredients' => $repositoryI->all()->sortBy('name'),
-                        'states' => $repositoryS->all()->sortBy('name'),
-                        'types' => $types,
-                        'items' => $items
-            ]);
+            return addMenuItens($request, $app);
         }, 'meals-itens.add')
         ->post('/meals-itens/store', function(ServerRequestInterface $request) use($app) {
             $data = $request->getParsedBody();
             $repository = $app->service('meal-item.repository');
-            $model = $repository->create($data);
+
+            try {
+                $model = $repository->create($data);
+            } catch (ValidatorException $exc) {
+                return addMenuItens($request, $app, [
+                    'errors' => $exc->getErrorMessages(),
+                    'data' => $data
+                ]);
+            } catch (\Exception $exc) {
+                return addMenuItens($request, $app, [
+                    'data' => $data,
+                    'errors' => [[
+                    'Algo não saiu como esperado'
+                        ]]
+                ]);
+            }
+
             return $app->redirect('/meals-itens/' . $model->meal_id . '/add');
         }, 'meals-itens.store')
         ->get('/meals-itens/{id}/show', function(ServerRequestInterface $request) use($app) {
@@ -77,3 +70,26 @@ $app->get('/meals-itens/{id}/add', function(ServerRequestInterface $request) use
             return $app->json(['response' => 'ok']);
         }, 'meals-itens.del');
 
+function addMenuItens($request, $app, array $params = [])
+{
+    $view = $app->service('view.renderer');
+    $id = (int) isset($params['data']) ? $params['data']['meal_id'] : $request->getAttribute('id');
+    $repository = $app->service('meal.repository');
+    $repositoryI = $app->service('ingredient.repository');
+    $repositoryS = $app->service('state.repository');
+    $meal = $repository->findOneBy([
+        'id' => $id,
+    ]);
+
+    $repositoryItem = $app->service('meal-item.repository');
+    $items = $repositoryItem->findByField('meal_id', $meal->id);
+
+    return $view->render('meals-itens/add.html.twig', [
+                'meal' => $meal,
+                'ingredients' => $repositoryI->all()->sortBy('name'),
+                'states' => $repositoryS->all()->sortBy('name'),
+                'types' => MealsIten::getTypes(),
+                'items' => $items,
+                'errors' => isset($params['errors']) ? $params['errors'] : [],
+    ]);
+}
--- src/Cardapium/Models/MealsIten.php
+++ src/Cardapium/Models/MealsIten.php
@@ -3,10 +3,19 @@
 namespace Cardapium\Models;
 
 use Cardapium\Models\Validators\FillableValidatorInterface;
+use Cardapium\Models\Validators\RecordExists;
 use Illuminate\Database\Eloquent\Model;
+use Zend\Validator\GreaterThan;
+use Zend\Validator\InArray;
+use Zend\Validator\NotEmpty;
 
 class MealsIten extends Model implements FillableValidatorInterface
 {
+
+    const TYPE_PRINCIPAL = 1;
+    const TYPE_ACOMPANHAMENTO = 2;
+    const TYPE_SALADA = 3;
+    const TYPE_LEGUME = 4;
 
     // Mass Assignment
     protected $fillable = [
@@ -16,6 +25,18 @@ class MealsIten extends Model implements FillableValidatorInterface
         'meal_id',
         'quantity',
     ];
+    protected $fillableValidators;
+    protected static $types = array(
+        self::TYPE_PRINCIPAL => 'Principal',
+        self::TYPE_ACOMPANHAMENTO => 'Acompanhamento',
+        self::TYPE_SALADA => 'Salada',
+        self::TYPE_LEGUME => 'Legume',
+    );
+
+    public static function getTypes()
+    {
+        return self::$types;
+    }
 
     public function ingredients()
     {
@@ -29,12 +50,52 @@ class MealsIten extends Model implements FillableValidatorInterface
 
     public function prepareFillableValidators(array $options = [])
     {
-
+        $this->fillableValidators = [
+            'ingredient_id' => [
+                'validators' => [
+                    (new NotEmpty)->setMessage('Tipo de ingrediente não pode ser vazio'),
+                    new RecordExists([
+                        'table' => Ingredient::class,
+                        'field' => 'id'
+                            ]),
+                ]
+            ],
+            'type_id' => [
+                'validators' => [
+                            (new InArray(['haystack' => array_keys(self::getTypes())]))
+                            ->setMessage('Tipo não esperado')
+                ]
+            ],
+            'state_id' => [
+                'validators' => [
+                    (new NotEmpty)->setMessage('Estado não pode ser vazio'),
+                    new RecordExists([
+                        'table' => State::class,
+                        'field' => 'id'
+                            ]),
+                ]
+            ],
+            'meal_id' => [
+                'validators' => [
+                    (new NotEmpty)->setMessage('Refeição não pode ser vazio'),
+                    new RecordExists([
+                        'table' => Meal::class,
+                        'field' => 'id'
+                            ]),
+                ]
+            ],
+            'quantity' => [
+                'validators' => [
+                    (new NotEmpty)->setMessage('Quantidade não pode ser vazio'),
+                    (new GreaterThan(0))->setMessage('Quantidade deve ser maior que 0'),
+                ]
+            ],
+        ];
     }
 
     public function getFillableValidators()
     {
-
+        return $this->fillableValidators;
     }
-    
+
 }
